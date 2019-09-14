7f0444b14dae908b35608c1dd09ee88bfc8db5a8
ryanaguilar <raguilar@scoe.org>
2019-09-14 08:43:07 -0700

    Add pro and fantasy teams

--- base.py
+++ base.py
@@ -80,10 +80,13 @@ class Player(dict):
         self.name = self['player']['fullName']
         self.on_team_id = self['onTeamId']
         self.pro_team_id = self['player']['proTeamId']
-        self.jersey = self['jersey']
+        self.jersey = self['player']['jersey']
         self.stats = self['player']['stats']
         self.position_id  = self['player']['defaultPositionId']
-        self.bye_wk = self['player']['outlooks']['outlooksByeWeek']
+        try:
+            self.bye_wk = self['player']['outlooks']['outlooksByeWeek']
+        except KeyError:
+            self.bye_wk = '0'
         #self. = self['player']['stats'][-1]['stats'][]
         self.pts = self['player']['stats'][-1]['appliedTotal']
         self.avg_pts = self['player']['stats'][-1].get('appliedAverage', 0)
@@ -102,73 +105,91 @@ class Player(dict):
         self.tar = self['player']['stats'][-1]['stats'].get('58', 0)
         self.twopc = self['player']['stats'][-1]['stats'].get('62', 0)
 
+        self.position_name = self.position_map()
+        self.ff_team = self.ff_team_map()
+        self.pro_team = self.pro_team_map()
+
     def position_map(self):
         position_name = {
-            '1':'QB',
-            '2':'RB',
-            '3':'WR',
-            '4':'TE',
-            '9':'DT',
-            '11': 'LB',
-            '16': 'DST'
+            1:'QB',
+            2:'RB',
+            3:'WR',
+            4:'TE',
+            5:'K',
+            6:'IDP',
+            7: 'IDP',
+            8: 'IDP',
+            9: 'IDP',
+            10: 'IDP',
+            11: 'IDP',
+            12: 'IDP',
+            13: 'IDP',
+            14: 'IDP',
+            15: 'IDP',
+            16: 'DST'
 
         }
+        return position_name[self.position_id]
 
     def ff_team_map(self):
         ff_team_map = {
-            '0': 'FA',
-            '1':'',
-            '2':'FBI',
-            '3':'LEE',
-            '4':'CHUB',
-            '5':'',
-            '6':'BRY',
-            '7':'MATT',
-            '8':'ASS',
-            '9':'MPB',
-            '10':'',
-            '11':'',
-            '12':'GM'
-            '13':'LUER'
+            0: 'FA',
+            1:'KIM',
+            2:'FBI',
+            3:'LEE',
+            4:'CHUB',
+            5:'',
+            6:'BRY',
+            7:'MATT',
+            8:'ASS',
+            9:'MPB',
+            10:'GM',
+            11:'GGWL',
+            12:'HOG'
+            13:'LUER'
         }
+        return ff_team_map[self.on_team_id]
 
     def pro_team_map(self):
         pro_team_map = {
-            '0':'FA',
-            '1':'',
-            '2':'BUF',
-            '3':'CHI',
-            '4':'CIN',
-            '5':'CLE',
-            '6':'DAL',
-            '7':'',
-            '8':'',
-            '9':'',
-            '10':'TEN',
-            '11':'',
-            '12':'KC',
-            '13':'',
-            '14':'LAR',
-            '15':'',
-            '16':'',
-            '17':'NE',
-            '18':'',
-            '19':'',
-            '20':'NYJ',
-            '21':'PHI',
-            '22':'',
-            '23':'',
-            '24':'',
-            '25':'',
-            '26':'',
-            '27':'TB',
-            '28':'',
-            '29':'CAR',
-            '30':'',
-            '31':'',
-            '32':''
+            0:'FA',
+            1:'ATL',
+            2:'BUF',
+            3:'CHI',
+            4:'CIN',
+            5:'CLE',
+            6:'DAL',
+            7:'DEN',
+            8:'DET',
+            9:'GB',
+            10:'TEN',
+            11:'IND',
+            12:'KC',
+            13:'OAK',
+            14:'LAR',
+            15:'MIA',
+            16:'MIN',
+            17:'NE',
+            18:'NO',
+            19:'NYG',
+            20:'NYJ',
+            21:'PHI',
+            22:'ARI',
+            23:'PIT',
+            24:'LAC',
+            25:'SF',
+            26:'SEA',
+            27:'TB',
+            28:'WAS',
+            29:'CAR',
+            30:'JAX',
+            31:'',
+            32:'',
+            33:'BAL',
+            34:'HOU'
 
         }
+        return pro_team_map[self.pro_team_id]
 
     def load_to_sql(self, engine):
         pass
