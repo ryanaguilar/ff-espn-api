b4d6161f8e9e3eb91cda07a41dba2604dece0c52
ryanaguilar <raguilar@scoe.org>
2019-09-14 00:21:59 -0700

    add table and player class

--- base.py
+++ base.py
@@ -2,6 +2,8 @@ from ff_espn_api import League
 import pprint
 import requests
 import json
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Numeric
+import pandas
 
 league_id = 372479
 year = 2019
@@ -28,6 +30,36 @@ def get_stats(player, period):
             objectplayer['rushing_tds'] = item['stats']['25']
             print(item['appliedTotal'])
 
+def setup_weekly_table():
+    meta = MetaDatat()
+    mytable = Table('mytable', meta,
+                Column('id', Integer, primary_key=True),
+                Column('name', String),
+                Column('pts', Numeric),
+                Column('attempts', Integer),
+                Column('completed', Integer),
+                Column('passing_yds', Numeric),
+                    Column('completion_pct', Numeric),
+                    Column('intercetptions', Integer),
+                    Column('passing_tds', Integer),
+                    Column('rushes', Integer),
+                    Column('rushing_yds', Numeric),
+                    Column('rushing_tds', Integer),
+                    Column('rushing_tds', Integer),
+                    Column('fumbles', Integer),
+                    Column('receptions', Integer),
+                    Column('receiving_yds', Numeric),
+                    Column('receiving_tds', Integer),
+                    Column('tar', Integer),
+                    Column('twopc', Integer),
+                    Column('projection', Numeric),
+                    Column('opponent', String),
+                    Column('fteam', String),
+                    Column('onteam_status', String),
+                    Column('proteam', Integer)
+
+    )
+
 def create_dict():
     pass
 
@@ -40,46 +72,37 @@ def search_by_name(players, searchname):
             print(searchname + ' found')
             return player
 
-stats_map = {
-    '0':'attempts',
-    '1':'completions',
-    '10':'',
-    '11':'',
-    '12':'',
-    '13':'',
-    '14':'',
-    '155':'',
-    '175':''
-}
 
 class Player(dict):
     def __init__(self, playerlist, name):
         super(Player, self).__init__(search_by_name(playerlist, name))
         self.id = self['id']
         self.name = self['player']['fullName']
-        self.stats = self['player']['stats'][-1]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
-        #self. = self['player']['stats']['stats'][]
+        self.stats = self['player']['stats']
+        #self. = self['player']['stats'][-1]['stats'][]
+        self.pts = self['player']['stats'][-1]['appliedTotal']
+        self.avg_pts = self['player']['stats'][-1]['appliedAverage']
+        self.attempts = self['player']['stats'][-1]['stats'].get('0', 0)
+        self.completed = self['player']['stats'][-1]['stats'].get('1', 0)
+        self.passing_yds = self['player']['stats'][-1]['stats'].get('3', 0)
+        self.completion_pct = self['player']['stats'][-1]['stats'].get('21', 0)
+        self.interceptions = self['player']['stats'][-1]['stats'].get('20', 0)
+        self.passing_tds = self['player']['stats'][-1]['stats'].get('4', 0)
+        self.rushing_tds = self['player']['stats'][-1]['stats'].get('25', 0)
+        self.rushing_yds = self['player']['stats'][-1]['stats'].get('24', 0)
+        self.rushes = self['player']['stats'][-1]['stats'].get('23', 0)
+        self.recptions = self['player']['stats'][-1]['stats'].get('41', 0)
+        self.receiving_yds = self['player']['stats'][-1]['stats'].get('42', 0)
+        self.receiving_tds = self['player']['stats'][-1]['stats'].get('43', 0)
+        self.tar = self['player']['stats'][-1]['stats'].get('58', 0)
+        self.twopc = self['player']['stats'][-1]['stats'].get('62', 0)
+
+    def load_to_sql(self, engine):
+        pass
+
+
+table = Table(
+    "week1", metadata,
+    Column("id")
+)
 
